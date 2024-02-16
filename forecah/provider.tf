f2319a815fdae69241ab386b3cfa306be1cdff4a
mutthuappalaraju <arappalaraju@gmail.com>
2024-02-16 13:39:53 +0530

    foreac

--- forecah/provider.tf
+++ forecah/provider.tf
@@ -5,8 +5,15 @@ terraform {
       version = "5.35.0"
     }
   }
+
+  backend "s3" {
+    bucket = "2512raju-remote"
+    key    = "foreach"
+    region = "us-east-1"
+    dynamodb_table = "raju-dynamodb"
+  }
 }
 
-provider "aws" {
-  region = "us-east-1"
+  provider "aws" {
+    region = "us-east-1"
 }
